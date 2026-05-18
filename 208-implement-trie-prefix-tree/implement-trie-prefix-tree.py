36e94dda9ce0e8cb24153db6fa44b588486fbde8
karthick <karthickkarth777@gmail.com>
2026-05-18 21:23:06 +0530

    Time: 102 ms (40.12%) | Memory: 39.2 MB (14.13%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,53 @@
+class TrieNode:
+    
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    
+    def insert(self, word):
+
+        node = self.root
+
+        for char in word:
+
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.endOfWord = True
+
+
+    def search(self, word):
+
+        node = self.root
+
+        for char in word:
+
+            if char not in node.children:
+                return False
+
+            node = node.children[char]
+
+        return node.endOfWord
+
+
+    def startsWith(self, prefix):
+
+        node = self.root
+
+        for char in prefix:
+
+            if char not in node.children:
+                return False
+
+            node = node.children[char]
+
+        return True
