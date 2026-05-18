af0facc414b219f6014bc9f1349082abbc9264f3
karthick <karthickkarth777@gmail.com>
2026-05-18 22:35:02 +0530

    Time: 551 ms (61.61%) | Memory: 33.7 MB (26.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -11,9 +11,12 @@ class Solution:
             for j in range(1, n + 1):
 
                 if text1[i - 1] == text2[j - 1]:
+
                     dp[i][j] = 1 + dp[i - 1][j - 1]
 
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+                    dp[i][j] = max(dp[i - 1][j],
+                                   dp[i][j - 1])
 
         return dp[m][n]
